a1762103099a7493f0b8c2ebf25c34d958492bc2
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-10-26 20:00:12 +0200

    removed disabling suspense in  next.config and added component level suspense

--- app/thankYou/page.js
+++ app/thankYou/page.js
@@ -1,34 +1,41 @@
 "use client";
+
 import styles from "../page.module.css";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function thankYou() {
+function ThankYouContent() {
   const searchParams = useSearchParams();
   const value = searchParams.get("value");
 
   return (
-    <Suspense>
-      <div className={styles.thankYouCardContainer}>
-        <div className={styles.thanksCard}>
-          <Image
-            src={"/images/illustration-thank-you.svg"}
-            width={150}
-            height={100}
-            alt="thank you image"
-            className={styles.image}
-          />
-          <div className={styles.thanksDiv}>
-            <p>You selected {value} out of 5</p>
-          </div>
-          <h1 className={styles.h1Thanks}>Thank You!</h1>
-          <p className={styles.paragraph}>
-            We appreciate you taking the time to give a rating. If you ever need
-            more support, don’t hesitate to get in touch!
-          </p>
+    <div className={styles.thankYouCardContainer}>
+      <div className={styles.thanksCard}>
+        <Image
+          src={"/images/illustration-thank-you.svg"}
+          width={150}
+          height={100}
+          alt="thank you image"
+          className={styles.image}
+        />
+        <div className={styles.thanksDiv}>
+          <p>You selected {value} out of 5</p>
         </div>
+        <h1 className={styles.h1Thanks}>Thank You!</h1>
+        <p className={styles.paragraph}>
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don’t hesitate to get in touch!
+        </p>
       </div>
+    </div>
+  );
+}
+
+export default function ThankYou() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ThankYouContent />
     </Suspense>
   );
 }
